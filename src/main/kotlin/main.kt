6fee0e6c980b39700d08278432496816491793c7
Giuseppe Vetri <giuseppe@codingpizza.com>
2021-01-25 14:15:56 +0100

    Introducción a variables

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,19 @@
+
+const val PI : Double = 3.1416
+
+fun main(args: Array<String>) {
+
+    var dinero : Int = 10
+    println(dinero)
+    dinero = 5
+    println(dinero)
+
+    // Variable de solo lectura
+
+    val nombre : String = "Maria"
+    println(nombre)
+
+// Esto daria error :
+// nombre = "Rose"
+
+}
