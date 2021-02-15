7023e7162ebd19d57176d89695e2e070a54eb2d2
Giuseppe Vetri <giuseppe@codingpizza.com>
2021-02-15 19:17:26 +0100

    Modificadores y tipos de datos

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,8 @@
 
-const val PI : Double = 3.1416
+//private const val PI : Double = 3.1416
+
+internal const val PI : Double = 3.1416
+
 
 fun main(args: Array<String>) {
 
@@ -15,5 +18,17 @@ fun main(args: Array<String>) {
 
 // Esto daria error :
 // nombre = "Rose"
+
+    val boolean : Boolean = true
+    val numeroLargo : Long = 30000000000000000
+    val double : Double = 2.7182
+    val float : Float = 1.1f
+
+    val primerValor = 20
+    val segundoValor = 10
+//    val tercerValor = primerValor.minus(segundoValor)
+    val tercerValor = primerValor - segundoValor
+
+    println(tercerValor)
 
 }
