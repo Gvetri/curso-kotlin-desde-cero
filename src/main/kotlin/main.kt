756598e3a3d15982a61f3f98bec37f9f3b0533ca
Giuseppe Vetri <giuseppe@codingpizza.com>
2021-02-15 19:59:41 +0100

    Estructuras de control: When

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,29 +1,45 @@
 
 fun main(args: Array<String>) {
 
-    val nombre : String = "Maria"
-    println(nombre)
+    val nombreColor = "Amarillo"
 
-    // Comprobación de if
-    if (nombre.isNotEmpty()) println("El largo de nuestra variable nombre es ${nombre.length}")
-    else println("Error, nuestra variable esta vacia")
-
-    // Asignación de if
-    var mensaje : String
-    if (nombre.length > 8){
-        mensaje = "Tienes un nombre largo!"
-    } else {
-        mensaje = "Tienes un nombre corto"
+    when (nombreColor) {
+        "Amarillo" -> println("El amarillo es el color de la alegria")
+        "Rojo" -> println("El rojo simboliza el calor")
     }
-    println(mensaje)
 
-    // Asignación de if inmutable
-    val mensajeInmutable = if (nombre.length > 8) "Tienes un nombre largo!" else "Tienes un nombre corto"
-    println(mensajeInmutable)
+    // Combinado con else
+    when (nombreColor) {
+        "Amarillo" -> println("El amarillo es el color de la alegria")
+        "Rojo" -> println("El rojo simboliza el calor")
+        else -> {
+            println("No tengo información de ese color")
+        }
+    }
 
-    // Asignación de variable inmutable con else if
-    val mensajeInmutable2= if (nombre.length > 8) "Tienes un nombre largo!" else if(nombre.isEmpty()) "Este nombre esta vacio :(" else "Tienes un nombre corto"
-    println(mensajeInmutable2)
+    // Ejecutando el mismo codigo para dos elementos
+    when (nombreColor) {
+        "Amarillo" -> println("El amarillo es el color de la alegria")
+        "Rojo","Carmesi" -> println("El rojo simboliza el calor")
+        else -> {
+            println("No tengo información de ese color")
+        }
+    }
 
+    //Comparación de rangos
+    val httpCode = 200
+    when (httpCode) {
+        in 200..299 -> println("Todo ha ido bien")
+        in 400..500 -> println("Algo ha fallado")
+        else -> println("Código desconocido, algo ha fallado")
+    }
 
+    //Asignación
+    val tallaDeZapatos = 41
+    val mensaje = when (tallaDeZapatos){
+        41,43 -> "Tenemos disponible"
+        42,44-> "Apurate casi no nos quedan!"
+        45 -> "Lo siento no tenemos disponible"
+        else -> "Estos zapatos solo vienen en tallas 41,42,43,44,45"
+    }
 }
