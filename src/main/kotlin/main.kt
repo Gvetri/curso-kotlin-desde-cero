878b5f1127155336fcfb605e096a9c0b94b2b22e
Giuseppe Vetri <giuseppe@codingpizza.com>
2021-02-16 11:48:10 +0100

    Elvis operator

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,23 +1,7 @@
 
 fun main(args: Array<String>) {
 
-    // Como lanzar una excepción
-    // throw IllegalStateException("Error!")
-
-
-    // Controlando las excepciones
-    try {
-        throw IllegalStateException("Error!")
-    } catch (e: Exception){
-        println("Ha ocurrido un error")
-    } finally{
-        println("Ejecutando código final...")
-    }
-
-    // Try como expresion
-    val primerValor = 10
-    val segundoValor = 0
-    val resultado: Int = try { primerValor / segundoValor } catch ( e: Exception ) { 0 }
-    println(resultado)
+    var nombre : String? = null
+    val caracteresDeNombre = nombre?.length ?: 0
 
 }
