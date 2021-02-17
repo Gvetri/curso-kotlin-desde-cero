81d5b1464ad8ad12e16bd38e04d83371292b8526
Giuseppe Vetri <giuseppe@codingpizza.com>
2021-02-17 13:11:01 +0100

    Funciones de extensión

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,20 +1,11 @@
 
 fun main(args: Array<String>) {
+    val letraAleatoria = "En Platzi nunca paramos de aprender".randomCase()
+    println(letraAleatoria)
+}
 
-    //Creando un set inmutable
-    val vocalesRepetidas = setOf("a", "e", "i", "o", "u", "a", "e", "i", "o", "u")
-    println(vocalesRepetidas) // Resultado: [a, e, i, o, u]
-
-    //Creando set mutables
-    val numerosFavoritos = mutableSetOf(1,2,3,4)
-    numerosFavoritos.add(5)
-    println(numerosFavoritos) // Resultado: [1, 2, 3, 4, 5]
-
-    //Eliminado elementos de set
-    numerosFavoritos.remove(3)
-    println(numerosFavoritos) // Resultado: [1, 2, 4, 5]
-
-    //Eliminar dependiendo de una condición
-    numerosFavoritos.removeIf { numero -> numero.rem(2) != 0 }
-    println(numerosFavoritos)
+private fun String.randomCase(): String {
+    val numeroAleatorio = 0..99
+    val resultadoAleatorio = numeroAleatorio.random()
+    return if (resultadoAleatorio.rem(2) == 0) toUpperCase() else toLowerCase()
 }
