48cbfb953e27f57ba3c38fdfb3299876ee1d5e04
Giuseppe Vetri <giuseppe@codingpizza.com>
2021-02-17 13:37:53 +0100

    Scope functions

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,46 @@
 
 fun main(args: Array<String>) {
-    val letraAleatoria = "En Platzi nunca paramos de aprender".randomCase()
-    println(letraAleatoria)
+
+    // Let
+    var nombre: String? = null
+    nombre?.let { // Esto no se ejecutará
+        println("El nombre no es nulo, es $it")
+    }
+    nombre = "Giuseppe"
+    nombre?.let { // Esto se ejecutará
+        println("El nombre no es nulo, es $it")
+    }
+
+    //With
+    val usuario = "giuseppe@platzi.com"
+    val mensajeDeInicioSesion = with(usuario) {
+        iniciarSesion(this)
+        "Bienvenido $this"
+    }
+    println(mensajeDeInicioSesion)
+
+    //Run
+    val moviles = mutableListOf("Google Pixel 2XL","Google Pixel 4a","Huawei Redmi 9","Xiaomi mi A3").run {
+        removeIf { movil -> movil.contains("Google") }
+        this
+    }
+    println(moviles)
+
+    //Apply
+    val movilesSinGoogle = mutableListOf("Google Pixel 2XL","Google Pixel 4a","Huawei Redmi 9","Xiaomi mi A3").apply{
+            removeIf { movil -> movil.contains("Google") }
+        }
+    println(movilesSinGoogle)
+
+    //Also
+    val lista = mutableListOf("Google Pixel 2XL","Google Pixel 4a","Huawei Redmi 9","Xiaomi mi A3")
+        .also { lista -> println("Lista original $lista") }
+        .asReversed()
+
+    println(lista)
+
 }
 
-private fun String.randomCase(): String {
-    val numeroAleatorio = 0..99
-    val resultadoAleatorio = numeroAleatorio.random()
-    return if (resultadoAleatorio.rem(2) == 0) toUpperCase() else toLowerCase()
+fun iniciarSesion(usuario: String){
+    println("Iniciando sesión con usuario $usuario")
 }
