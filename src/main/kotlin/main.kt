ee8b95f9db16493844d071f902f6927d2c3b135e
Giuseppe Vetri <giuseppe@codingpizza.com>
2021-04-10 15:05:26 +0200

    Scope functions Apply

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,12 @@
 fun main(args: Array<String>) {
-    val moviles = mutableListOf("Google Pixel 2XL", "Google Pixel 4a", "Huawei Redmi 9", "Xiaomi mi a3")
-        .run {
-            removeIf { movil -> movil.contains("Google") }
-            this
-        }
+    val moviles = mutableListOf("Google Pixel 2XL", "Google Pixel 4a", "Huawei Redmi 9", "Xiaomi mi a3").apply {
+        removeIf { movil -> movil.contains("Google") }
+    }
     println(moviles)
+
+    val colores : MutableList<String>? = mutableListOf("Amarillo","Azul","Rojo")
+    colores?.apply {
+        println("Nuestros colores son $this")
+        println("la cantidad de colores es $size")
+    }
 }
